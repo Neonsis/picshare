a04379e23a4a91119e2d7d839860f5c6d499b33a
Andrey Vinel <vinel.work03@gmail.com>
2020-08-28 13:36:34 +0300

    Add mapping.xml

--- picshare-common/src/main/java/org/neonsis/picshare/common/annotation/cdi/PropertiesSource.java
+++ picshare-common/src/main/java/org/neonsis/picshare/common/annotation/cdi/PropertiesSource.java
@@ -2,12 +2,10 @@ package org.neonsis.picshare.common.annotation.cdi;
 
 import javax.enterprise.util.Nonbinding;
 import javax.inject.Qualifier;
-
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.*;
-import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Qualifier
